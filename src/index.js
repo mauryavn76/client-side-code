22e1d3649e21c95914d53afe01e898eb0f323ef5
mauryavn76 <51995096+mauryavn76@users.noreply.github.com>
2019-07-29 14:49:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
+//added comment for testing pipeline
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
